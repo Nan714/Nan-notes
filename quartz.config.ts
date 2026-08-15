e10b8b307d3a7dd1ee1204e4b2caa80679890916
Nan <nanw70104@gmail.com>
2026-08-15 13:43:59 +0800

    站点改名为「半熟」，扩充首页内容

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "途中笔记",
+    pageTitle: "半熟",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
